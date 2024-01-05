86fdb374aa880525f3f2b55fa6da955ca6c39b9b
magfradeveloper <oscarmagallon@hotmail.com>
2024-01-04 20:34:15 -0600

    Primera publicación

--- App.js
+++ App.js
@@ -1,19 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {YellowBox} from "react-native";
+import {LogBox} from "react-native";
+import Navigation from "./app/navigations/Navigation";
+import { firebaseApp } from "./app/utils/firebase";
+import * as firebase from "firebase";
+
+//YellowBox.ignoreWarnings(["Setting a timer", "expo-permissions", "It appears that you are using old version of react-navigation library"]);
+LogBox.ignoreLogs(['Warning: ...']);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <Navigation></Navigation>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
